67e9419d99c98071f14bb99a91dea403b9f0db99
Cookie <42448678+CookiePPP@users.noreply.github.com>
2019-12-29 05:45:15 +0000

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -19,7 +19,7 @@ def create_hparams(hparams_string=None, verbose=False):
         dist_url="tcp://localhost:54321",
         cudnn_enabled=True,
         cudnn_benchmark=False,
-        ignore_layers=list(),
+        ignore_layers=[0],
 
         ################################
         # Data Parameters             #
