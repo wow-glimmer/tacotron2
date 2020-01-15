8520e069c534b230ef5cd54f4bb515a9f2420f7b
Cookie <42448678+CookiePPP@users.noreply.github.com>
2020-01-15 02:54:26 +0000

    initial

--- logger.py
+++ logger.py
@@ -4,7 +4,6 @@ from tensorboardX import SummaryWriter
 from plotting_utils import plot_alignment_to_numpy, plot_spectrogram_to_numpy
 from plotting_utils import plot_gate_outputs_to_numpy
 
-
 class Tacotron2Logger(SummaryWriter):
     def __init__(self, logdir):
         super(Tacotron2Logger, self).__init__(logdir)
@@ -19,8 +18,10 @@ class Tacotron2Logger(SummaryWriter):
             self.add_scalar("learning.rate", learning_rate, iteration)
             self.add_scalar("duration", duration, iteration)
 
-    def log_validation(self, reduced_loss, model, y, y_pred, iteration):
+    def log_validation(self, reduced_loss, model, y, y_pred, iteration, diagonality, avg_prob):
         self.add_scalar("validation.loss", reduced_loss, iteration)
+        self.add_scalar("validation.attention_alignment_diagonality", diagonality, iteration)
+        self.add_scalar("validation.average_max_attention_weight", avg_prob, iteration)
         _, _, mel_outputs, gate_outputs, alignments = y_pred
         mel_targets, gate_targets = y
 
@@ -28,9 +29,9 @@ class Tacotron2Logger(SummaryWriter):
         for tag, value in model.named_parameters():
             tag = tag.replace('.', '/')
             self.add_histogram(tag, value.data.cpu().numpy(), iteration)
-
+        
         # plot alignment, mel target and predicted, gate target and predicted
-        idx = random.randint(0, alignments.size(0) - 1)
+        idx = 0 # plot longest audio file
         self.add_image(
             "alignment",
             plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
@@ -45,6 +46,26 @@ class Tacotron2Logger(SummaryWriter):
             iteration, dataformats='HWC')
         self.add_image(
             "gate",
+            plot_gate_outputs_to_numpy(
+                gate_targets[idx].data.cpu().numpy(),
+                torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        
+        idx = random.randint(0, alignments.size(0) - 3) # and randomly pick a second one to plot
+        self.add_image(
+            "alignment2",
+            plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_target2",
+            plot_spectrogram_to_numpy(mel_targets[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_predicted2",
+            plot_spectrogram_to_numpy(mel_outputs[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "gate2",
             plot_gate_outputs_to_numpy(
                 gate_targets[idx].data.cpu().numpy(),
                 torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
