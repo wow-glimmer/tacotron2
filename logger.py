d014e3f24a282369f56ff412e015f2631970a97c
Cookie <42448678+CookiePPP@users.noreply.github.com>
2020-01-31 23:22:33 +0000

    Add files via upload

--- logger.py
+++ logger.py
@@ -4,33 +4,35 @@ from tensorboardX import SummaryWriter
 from plotting_utils import plot_alignment_to_numpy, plot_spectrogram_to_numpy
 from plotting_utils import plot_gate_outputs_to_numpy
 
-
 class Tacotron2Logger(SummaryWriter):
     def __init__(self, logdir):
         super(Tacotron2Logger, self).__init__(logdir)
 
-    def log_training(self, total_loss, taco_loss, mi_loss, grad_norm,
-                     gaf, learning_rate, duration, iteration):
-            self.add_scalar("training.loss", total_loss, iteration)
-            self.add_scalar("training.taco_loss", taco_loss, iteration)
-            self.add_scalar("training.mi_loss", mi_loss, iteration)
+    def log_training(self, reduced_loss, grad_norm, learning_rate, duration,
+                     iteration, diagonality=None, avg_prob=None):
+            if not (diagonality is None):
+                self.add_scalar("training.attention_alignment_diagonality", diagonality, iteration)
+            if not (avg_prob is None):
+                self.add_scalar("training.average_max_attention_weight", avg_prob, iteration)
+            self.add_scalar("training.loss", reduced_loss, iteration)
             self.add_scalar("grad.norm", grad_norm, iteration)
-            self.add_scalar("grad.gaf", gaf, iteration)
             self.add_scalar("learning.rate", learning_rate, iteration)
             self.add_scalar("duration", duration, iteration)
 
-    def log_validation(self, reduced_loss, model, y, y_pred, iteration):
+    def log_validation(self, reduced_loss, model, y, y_pred, iteration, diagonality, avg_prob):
         self.add_scalar("validation.loss", reduced_loss, iteration)
-        _, _, mel_outputs, gate_outputs, alignments = y_pred
+        self.add_scalar("validation.attention_alignment_diagonality", diagonality, iteration)
+        self.add_scalar("validation.average_max_attention_weight", avg_prob, iteration)
+        _, mel_outputs, gate_outputs, alignments = y_pred
         mel_targets, gate_targets = y
 
         # plot distribution of parameters
         for tag, value in model.named_parameters():
             tag = tag.replace('.', '/')
             self.add_histogram(tag, value.data.cpu().numpy(), iteration)
-
+        
         # plot alignment, mel target and predicted, gate target and predicted
-        idx = random.randint(0, alignments.size(0) - 1)
+        idx = 0 # plot longest audio file
         self.add_image(
             "alignment",
             plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
@@ -45,6 +47,66 @@ class Tacotron2Logger(SummaryWriter):
             iteration, dataformats='HWC')
         self.add_image(
             "gate",
+            plot_gate_outputs_to_numpy(
+                gate_targets[idx].data.cpu().numpy(),
+                torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        
+        idx = 1 # and randomly pick a second one to plot
+        self.add_image(
+            "alignment2",
+            plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_target2",
+            plot_spectrogram_to_numpy(mel_targets[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_predicted2",
+            plot_spectrogram_to_numpy(mel_outputs[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "gate2",
+            plot_gate_outputs_to_numpy(
+                gate_targets[idx].data.cpu().numpy(),
+                torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
+            iteration, dataformats='HWC')
+
+        idx = 2 # and randomly pick a second one to plot
+        self.add_image(
+            "alignment3",
+            plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_target3",
+            plot_spectrogram_to_numpy(mel_targets[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_predicted3",
+            plot_spectrogram_to_numpy(mel_outputs[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "gate3",
+            plot_gate_outputs_to_numpy(
+                gate_targets[idx].data.cpu().numpy(),
+                torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        
+        idx = 3 # and randomly pick a second one to plot
+        self.add_image(
+            "alignment4",
+            plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_target4",
+            plot_spectrogram_to_numpy(mel_targets[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "mel_predicted4",
+            plot_spectrogram_to_numpy(mel_outputs[idx].data.cpu().numpy()),
+            iteration, dataformats='HWC')
+        self.add_image(
+            "gate4",
             plot_gate_outputs_to_numpy(
                 gate_targets[idx].data.cpu().numpy(),
                 torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
